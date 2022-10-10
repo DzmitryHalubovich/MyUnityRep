bb719dd577c637a5b26ad29d2f3ac2f74ce366ea
DzmitryHalubovich <buxarik493@gmail.com>
2022-10-10 23:39:59 +0300

    add sounds, still need for enemies

--- Assets/Scripts/PlayerBehavior/Shoot.cs
+++ Assets/Scripts/PlayerBehavior/Shoot.cs
@@ -6,10 +6,13 @@ public class Shoot : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public ParticleSystem shootParticleEffects;
+
+    private AudioSource playerAudio;
+    public AudioClip shootSound;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,6 +24,7 @@ public class Shoot : MonoBehaviour
             Vector3 position = new Vector3(transform.position.x,transform.position.y,transform.position.z+10);
             Instantiate(bulletPrefab, position, bulletPrefab.transform.rotation);
             shootParticleEffects.Play();
+            playerAudio.PlayOneShot(shootSound, 0.4f);
         }
     }
 }
